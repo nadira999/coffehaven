3ec8a392f1d80bc0a4fba55af2aa0efe3f7563a9
nadira tri maylani <bundatriwati051279@gmail.com>
2026-08-31 11:32:03 +0700

    merevisi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(function (Request $request) {
+            return route('owner.login');
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
